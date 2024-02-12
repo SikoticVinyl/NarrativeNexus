03598f0b3ad0cc07d5b8cd1dc2229dfc3678c8ea
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-12 16:26:33 -0700

    recoded savedbooks.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
+import { 
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProvider, 
+  createHttpLink 
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './App.css';
+import Auth from './utils/auth';
 
-// Initialize Apollo Client
+// Create an HTTP link that connects to the GraphQL endpoint
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+});
+
+// Create a context link that adds the token to the headers of each request
+const authLink = setContext((_, { headers }) => {
+  // Get the authentication token from local storage if it exists
+  const token = Auth.getToken();
+  // Return the headers to the context so the httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  };
+});
+
+// Initialize Apollo Client with the authLink and httpLink
 const client = new ApolloClient({
-  link: new HttpLink({ uri: process.env.REACT_APP_GRAPHQL_ENDPOINT }),
+  link: authLink.concat(httpLink), // Chain the authLink with the httpLink
   cache: new InMemoryCache(),
 });
 
