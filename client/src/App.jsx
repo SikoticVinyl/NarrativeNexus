e73fd109677f4872cde73dc9c0538e5155d3c9c1
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-12 15:58:35 -0700

    brought apollo into app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,25 @@
-import './App.css';
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+import './App.css';
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  link: new HttpLink({ uri: process.env.REACT_APP_GRAPHQL_ENDPOINT }),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <div>
+        <Navbar />
+        <Outlet />
+      </div>
+    </ApolloProvider>
   );
 }
 
 export default App;
+
