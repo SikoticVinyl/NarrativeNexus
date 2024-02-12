9f9aec33ea85d396dde5c8ae8ab734f22763fe4c
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-12 15:49:10 -0700

    created login and create user mutations on frontend.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,27 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($loginInput: String!, $password: String!) {
+    login(loginInput: $loginInput, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
