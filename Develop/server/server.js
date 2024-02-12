974f7923206d96dec27adbeb9fed6b4af0d28c8d
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-02-11 20:52:26 -0700

    update server.js for graphql.

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,39 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { } = require('./schemas'); //not yet created
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  //schemas here
+  context: authMiddleware,
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
+startServer();
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`🚀 GraphQL API ready at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
